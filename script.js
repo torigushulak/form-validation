6d483ac04e150dd8fa21df44a3fe024c3e3ca7af
Tori Gushulak <tori.gushulak@gmail.com>
2024-02-27 14:36:48 -0700

    form verification done

--- script.js
+++ script.js
@@ -1,32 +1,10 @@
-function checkEmail() {
-  let email = document.forms[myForm]["email"].value;
-  if (!email.value) {
-    email.style.border = '5px solid';
-    email.style.borderColor = 'red';
-  }
-}
 
-function checkCountry() {
-  let country = document.forms[myForm]["country"].value;
-  if (!country.value) {
-    country.style.border = '5px solid';
-    country.style.borderColor = 'red';
-  }
-}
-
-function checkZip() {
-  let zip = document.forms[myForm]["zip"].value;
-  
-  if (!zip.value) {
-    zip.style.border = '5px solid';
-    zip.style.borderColor = 'red';
-  }
-}
-
-function checkPassword() {
-  let password = document.forms[myForm]["password"].value;
-  if (!password.value) {
-    password.style.border = '5px solid';
-    password.style.borderColor = 'red';
+function validateMyForm() {
+  let password = document.getElementById('password').value;
+  let passwordConfirm = document.getElementById('passwordConfirm').value;
+  if (password !== passwordConfirm) {
+    alert('passwords do not match');
+  } else {
+   $("#submit").unbind('click').click(); 
   }
 }
